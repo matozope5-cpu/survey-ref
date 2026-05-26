5dd012491cff5f89b91396907a2c0e7fd478dcc4
matozope5-cpu <matozope5@gmail.com>
2026-05-26 22:55:55 +0300

    first-rollback-6

--- api/initiate-payment.js
+++ api/initiate-payment.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
 
     const PAYHERO_CONFIG = {
       apiUrl: 'https://backend.payhero.co.ke/api/v2/payments',
-      basicAuthToken: 'Basic bHd6MkhZUW4xODJCNEJTaGxpdkg6ZE1vZHQzM3F3bmRXSG5JalFPakRPOWFub3RRc2xXUTRBVUhPNEpSWQ==',
-      channelId: 6241,
+      basicAuthToken: 'Basic cXBMUWhyeHU3SW41b2xuaDFRTWM6OWUyYWoyRXZZV1N0T2VMUWhjU3VZd1dSSDN4VTNrVGxZQWhWY2c1RA==',
+      channelId: 4270,
       provider: 'm-pesa',
       callbackUrl: 'https://samttech.co.ke/callback'
     };
